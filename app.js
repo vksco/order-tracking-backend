77c6e807d7ed1c03b5bdcc11292285cf6ddcd7c0
Vikash <vikash@karmicksolutions.com>
2025-03-03 13:45:47 +0530

    added vscode settings

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const indexRoutes = require('./routes/index');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
@@ -12,11 +11,14 @@ connectDB();
 // Middleware
 app.use(express.json());
 
+// Route handlers
+const userRoutes = require('./routes/userRoutes');
+const indexRoutes = require('./routes/index');
+
 // Routes
-app.use('/api', userRoutes);
+app.use('/api/users', userRoutes);
 app.use('/', indexRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { createUser } = require('../controllers/userController');
 const router = express.Router();
 
-router.post('/users', createUser);
+router.post('/', createUser);
 
 module.exports = router;
