3b613bc46d7ca1a18e78ce55bc4cfb89f686d6cd
Vikash <vikash@karmicksolutions.com>
2025-03-03 13:56:40 +0530

    project setup done - 3

--- helpers/api_response.js
+++ helpers/api_response.js
@@ -1,3 +1,10 @@
+/**
+ * Sends a success response with a 200 status code.
+ * @param {Object} res - The response object.
+ * @param {string} message - The success message.
+ * @param {Object} [data={}] - The data to include in the response.
+ * @returns {Object} - The JSON response.
+ */
 exports.successResponse = (res, message, data = {}) => {
     return res.status(200).json({
         success: true,
@@ -6,6 +13,13 @@ exports.successResponse = (res, message, data = {}) => {
     });
 };
 
+/**
+ * Sends an error response with a specified status code.
+ * @param {Object} res - The response object.
+ * @param {string} message - The error message.
+ * @param {number} [statusCode=400] - The status code to use for the response.
+ * @returns {Object} - The JSON response.
+ */
 exports.errorResponse = (res, message, statusCode = 400) => {
     return res.status(statusCode).json({
         success: false,
